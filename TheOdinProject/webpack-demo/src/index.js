b38af7eea81d400098b487b643916cc9bf7d4695
Enzzx <bellottie815@gmail.com>
2023-02-25 03:12:44 -0300

    update

--- TheOdinProject/webpack-demo/src/index.js
+++ TheOdinProject/webpack-demo/src/index.js
@@ -1,11 +1,23 @@
 import _ from 'lodash'
-import myName from './myName'
+import './style.css'
+import icon from './icon.jpg'
+import Data from './data.xml'
+import Notes from './data.csv'
 
 function component() {
     const element = document.createElement('div')
 
-    element.textContent = myName('Enzo')
+    element.textContent = _.join(['hello', 'webpack'], ' ')
+    element.classList.add('hello')
 
+    const myImage = new Image()
+    myImage.src = icon
+
+    element.appendChild(myImage)
+
+    console.log(Data)
+    console.log(Notes)
+    
     return element
 }
 
